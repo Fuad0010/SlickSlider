75f36257aa70627cc695dd4c2adc5913be493c11
Fuad0010 <fuadft@code.edu.az>
2022-09-10 13:52:22 +0400

    feat:addded slick slider

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        nextArrow:'<div class="next_Arrow"><i class="fa-solid fa-angle-right"></i></div>',
+        prevArrow:'<div class="prev_Arrow"><i class="fa-solid fa-angle-left"></i></div>'
+
+    });      
+});
